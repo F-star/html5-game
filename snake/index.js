e33cbab65c5e14e336b1e9e866fc7efc1a83cca0
fstar-mac <hhbuhuiai@163.com>
2019-12-26 00:19:36 +0800

    feat: 加了一个黄色的苹果

--- snake/index.js
+++ snake/index.js
@@ -18,11 +18,13 @@ class DataView {
 
 //层
 class Layer {
-  constructor(width, height) {
+  constructor(width, height, grid_w) {
     this.el = document.createElement('canvas');
     this.ctx = this.el.getContext('2d');
-    this.snake = undefined;
+    this.snake = null;
+    this.apple = null;
     this.size(width, height);
+    this.grid_w = grid_w;
 
     this.dataView = new DataView();
     this.last_ts = 0;
@@ -49,10 +51,16 @@ class Layer {
   addSnake(snake) {
     this.snake = snake;
   }
+  addApple(apple) {
+    this.apple = apple;
+  }
   clear() {
     this.ctx.clearRect(0, 0, this.width(), this.height());
   }
   start() {
+    this.apple.setPosExcept(this.snake.row, this.snake.col, this.snake.points);
+    this.apple.draw(this.ctx);
+
     this.update();
   }
   update() {
@@ -60,11 +68,15 @@ class Layer {
       // dt 为当前帧和上一帧的时间差。单位为 秒/帧，fps = 1/dt
       if (this.last_ts === 0) this.last_ts = ts;
       const dt = ts - this.last_ts; // 两帧的间隔时间
-      if (this.count++ % 5 == 0) {
+      if (this.count++ % 10 == 0) {
         this.dataView.updateFPS('fps: ' + 1 / dt * 1000);
         this.clear();
-        snake.update();
-        snake.draw(this.ctx);
+        const eat = this.snake.update(apple);
+        if (eat) {
+          this.apple.setPosExcept(this.snake.row, this.snake.col, this.snake.points);
+        }
+        this.apple.draw(this.ctx);
+        this.snake.draw(this.ctx, this.grid_w);
       }
       this.last_ts = ts;
       this.update();
@@ -88,8 +100,12 @@ class Snake {
   constructor(grid_w, points, dirStr, width, height) {
     this.grid_w = grid_w;
     this.points = points; // 方向为：头到尾
-    this.row = width / grid_w;
-    this.col = height / grid_w;
+    this.row = height / grid_w;
+    this.col = width / grid_w;
+    console.log({
+      row: this.row,
+      col: this.col
+    });
     this.dir = this.getDirVectorByStr(dirStr);
     this.next_dir = null;
     this.speed_ = 0; // 每秒移动几个单位。
@@ -110,13 +126,6 @@ class Snake {
     }
     this.speed_ = val;
   }
-  draw(ctx) {
-    this.points.forEach(point => {
-      const x = point.x * this.grid_w;
-      const y = point.y * this.grid_w;
-      ctx.fillRect(x, y, this.grid_w, this.grid_w);
-    });
-  }
   getDirVectorByStr(dirStr) {
     const vector = {
       up:    {x: 0,  y: -1},
@@ -128,26 +137,102 @@ class Snake {
     return new Vector(vector.x, vector.y);
   }
   // 设置方向。
-  setDir(dirStr) {
+  setNextDir(dirStr) {
     const next_dir = this.getDirVectorByStr(dirStr);
-    if (this.dir.dotProduct(next_dir) < 0) return false; // 反向，设置失败
-    this.dir = next_dir;
-    return true;
+    this.next_dir = next_dir;
   }
   // 更新位置信息。
-  update() {
+  update(applePos) {
+    if (this.next_dir && this.next_dir.dotProduct(this.dir) >= 0) { // next_dir 不和 dir 反向
+      this.dir = this.next_dir;
+      this.next_dir = null;
+    }
     const head = this.points[0];
     const next_head = {
-      x: (head.x + this.dir.x + this.row) % this.row,
-      y: (head.y + this.dir.y + this.col) % this.col
+      x: (head.x + this.dir.x + this.col) % this.col,
+      y: (head.y + this.dir.y + this.row) % this.row
     };
     this.points.unshift(next_head);
-    this.points.pop();
+    let eat;
+    if (next_head.x === applePos.x && next_head.y === applePos.y){
+      eat = true;
+    } else {
+      this.points.pop();
+      eat = false;
+    }
+    return eat;
+  }
+  draw(ctx) {
+    this.points.forEach((point, index) => {
+      const x = point.x * this.grid_w;
+      const y = point.y * this.grid_w;
+      if (index === 0) ctx.fillStyle = 'red';
+      else ctx.fillStyle = '#000';
+      ctx.fillRect(x, y, this.grid_w, this.grid_w);
+    });
   }
 }
 
-const layer = new Layer(600, 500);
+// 苹果
+class Apple {
+  constructor(grid_w) {
+    this.x = 0;
+    this.y = 0;
+    this.grid_w = grid_w;
+  }
+  // 设置一个随机坐标，要求在 [col, row] 内且不在 points 内。
+  setPosExcept(row, col, points) {
+    const obj = {};
+    points.forEach(point => {
+      if (!obj[point.x]) obj[point.x] = [];
+      obj[point.x].push(point.y);
+    });
+    let space_sum = row * col - points.length; // 总空白格数
+    let random_index = Math.floor(Math.random() * (space_sum + 1));
+    console.log('随机数：', random_index);
+    let x, y;
+    let found = false;
+    let col_space_count; // 列空白格数
+    for (let i = 0; i < col; i++) {
+      if (found) break;
+      col_space_count = obj[i] ? row - obj[i].length : row; // 当前列的空格数
+      if (random_index < col_space_count) {
+        // 确定 x
+        x = i;
+        found = true;
+        for (let j = 0; j < row; j++) {
+          if (random_index === 0) {
+            // 确定 y 坐标
+            y = j;
+            break;
+          }
+          if (!obj[i] || !obj[i].includes(j)) random_index--;
+        }
+      } else {
+        random_index -= col_space_count;
+      }
+    }
+    this.x = x;
+    this.y = y;
+    console.log(
+      '苹果位置：', x, y
+    );
+  }
+  draw(ctx) {
+    ctx.save();
+    ctx.fillStyle = 'yellow';
+    ctx.fillRect(this.x * this.grid_w, this.y * this.grid_w, this.grid_w, this.grid_w);
+    ctx.restore();
+  }
+}
+
+const grid_w = 20; // 格子大小
+const width = 400;
+const height = 400;
+
+const layer = new Layer(width, height, grid_w);
 document.querySelector('#view').appendChild(layer.el);
+
 const points = [
   { x: 2, y: 3 },
   { x: 2, y: 2 },
@@ -155,18 +240,19 @@ const points = [
   { x: 1, y: 1 },
   { x: 0, y: 1 },
 ];
-const snake = new Snake(20, points, 'down', 600, 500);
-snake.speed(2); // 每秒移动两个单位。
+const snake = new Snake(grid_w, points, 'down', width, height);
+const apple = new Apple(grid_w);
 layer.addSnake(snake);
+layer.addApple(apple);
 layer.start();
 
 window.addEventListener('keydown', function(e) {
   if (e.code === 'ArrowLeft' || e.keyCode === 37)
-    snake.setDir('left'); 
+    snake.setNextDir('left'); 
   else if (e.code === 'ArrowUp' || e.keyCode === 38)
-    snake.setDir('up'); 
+    snake.setNextDir('up'); 
   else if (e.code === 'ArrowRight' || e.keyCode === 39)
-    snake.setDir('right');
+    snake.setNextDir('right');
   else if (e.code === 'ArrowDown' || e.keyCode === 40)
-    snake.setDir('down');
+    snake.setNextDir('down');
 });
