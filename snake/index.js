1ae945666e774035665cee644a1e6a06459c29fc
fstar-mac <hhbuhuiai@163.com>
2019-12-26 23:50:46 +0800

    允许使用 WASD 键控制方向； 尝试修复了苹果重新生成位置会在贪吃蛇上的bug； 添加吃掉的苹果树显示

--- snake/index.js
+++ snake/index.js
@@ -6,16 +6,6 @@
   }
 } */
 
-class DataView {
-  constructor() {
-    this.el = document.createElement('div');
-    document.body.append(this.el);
-
-  }
-  updateFPS(fps) {
-    this.el.innerHTML = fps;
-  }
-}
 
 //层
 class Layer {
@@ -26,10 +16,9 @@ class Layer {
     this.apple = null;
     this.size(width, height);
     this.grid_w = grid_w;
-
-    this.dataView = new DataView();
     this.last_ts = 0;
-    this.count = 0;
+    this.frame_count = 0;
+    this.score = 0;
   }
   width(val) {
     if (val == undefined) {
@@ -55,6 +44,9 @@ class Layer {
   addApple(apple) {
     this.apple = apple;
   }
+  addDataViewer(dataViewer) {
+    this.dataViewer = dataViewer;
+  }
   clear() {
     this.ctx.clearRect(0, 0, this.width(), this.height());
   }
@@ -69,25 +61,29 @@ class Layer {
       // dt 为当前帧和上一帧的时间差。单位为 秒/帧，fps = 1/dt
       if (this.last_ts === 0) this.last_ts = ts;
       const dt = ts - this.last_ts; // 两帧的间隔时间
-      if (this.count++ % 10 == 0) {
-        this.dataView.updateFPS('fps: ' + 1 / dt * 1000);
+      if (this.frame_count++ % 10 == 0) {
         const next_head = this.snake.getNextHeadPos();
+        
         // 吃到苹果
-        let eat = false;
         if (next_head.x === apple.x && next_head.y === apple.y){
+          this.score++;
+          this.snake.update(next_head, true);
           this.apple.setPosExcept(this.snake.row, this.snake.col, this.snake.points);
-          eat = true;
+        } else {
+          this.snake.update(next_head, false);
         }
-        this.snake.update(next_head, eat);
+        
         if (this.snake.isHitSelf()) {
           return this.stop();
         }
 
         // 渲染
         this.clear();
-
         this.snake.draw(this.ctx, this.grid_w);
         this.apple.draw(this.ctx);
+        // 数据
+        this.dataViewer.fps( (1 / dt * 1000).toFixed(2) );
+        this.dataViewer.score(this.score);
       }
       this.last_ts = ts;
       this.update();
@@ -177,15 +173,6 @@ class Snake {
   }
   // 更新位置信息。
   update(next_head, grow) {
-    /* if (this.next_dir && this.next_dir.dotProduct(this.dir) >= 0) { // next_dir 不和 dir 反向
-      this.dir = this.next_dir;
-      this.next_dir = null;
-    }
-    const head = this.points[0];
-    const next_head = {
-      x: (head.x + this.dir.x + this.col) % this.col,
-      y: (head.y + this.dir.y + this.row) % this.row
-    }; */
     this.points.unshift(next_head);
     if (!grow) this.points.pop();
   }
@@ -216,7 +203,6 @@ class Apple {
     });
     let space_sum = row * col - points.length; // 总空白格数
     let random_index = Math.floor(Math.random() * (space_sum + 1));
-    console.log('随机数：', random_index);
     let x, y;
     let found = false;
     let col_space_count; // 列空白格数
@@ -241,15 +227,27 @@ class Apple {
     }
     this.x = x;
     this.y = y;
-    console.log(
-      '苹果位置：', x, y
-    );
   }
   draw(ctx) {
     ctx.save();
     ctx.fillStyle = 'yellow';
     ctx.fillRect(this.x * this.grid_w, this.y * this.grid_w, this.grid_w, this.grid_w);
     ctx.restore();
+  }
+}
+
+class DataViewer {
+  constructor() {
+    this.fps_el = document.createElement('div');
+    this.score_el = document.createElement('div');
+    document.body.append(this.score_el);
+    document.body.append(this.fps_el);
+  }
+  fps(fps) {
+    this.fps_el.innerText = `fps: ${fps}`;
+  }
+  score(score) {
+    this.score_el.innerText = `分数: ${score}`;
   }
 }
 
@@ -269,17 +267,23 @@ const points = [
 ];
 const snake = new Snake(grid_w, points, 'down', width, height);
 const apple = new Apple(grid_w);
+const dataViewer = new DataViewer();
 layer.addSnake(snake);
 layer.addApple(apple);
+layer.addDataViewer(dataViewer);
 layer.start();
 
 window.addEventListener('keydown', function(e) {
-  if (e.code === 'ArrowLeft' || e.keyCode === 37)
+  if (e.code === 'KeyA' || e.keyCode === 65 ||
+    e.code === 'ArrowLeft' || e.keyCode === 37)
     snake.setNextDir('left'); 
-  else if (e.code === 'ArrowUp' || e.keyCode === 38)
+  else if (e.code === 'KeyW' || e.keyCode === 87 ||
+    e.code === 'ArrowUp' || e.keyCode === 38)
     snake.setNextDir('up'); 
-  else if (e.code === 'ArrowRight' || e.keyCode === 39)
+  else if (e.code === 'KeyD' || e.keyCode === 68 ||
+    e.code === 'ArrowRight' || e.keyCode === 39)
     snake.setNextDir('right');
-  else if (e.code === 'ArrowDown' || e.keyCode === 40)
+  else if (e.code === 'KeyS' || e.keyCode === 83 ||
+    e.code === 'ArrowDown' || e.keyCode === 40)
     snake.setNextDir('down');
 });
