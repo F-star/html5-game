79d323b0a5f9cbcde0320d3c4dea365477f56b03
fstar <huanghao2@xiao100.com>
2020-03-05 13:11:43 +0800

    update

---
+++ Sokoban/src/GameHistory.ts
@@ -0,0 +1,29 @@
+/**
+ * 历史记录
+ */
+class GameHistory {
+  // 双向链表
+  private DoublyLinkedList = [];
+  private size = 0;
+  constructor() {}
+
+  
+}
+
+
+
+// 命令接口
+interface Command {
+  exec(): void
+  undo(): void
+}
+
+
+
+/**
+ * 双向链表（接口）
+ */
+// https://www.typescriptlang.org/docs/handbook/interfaces.html#class-types
+interface IDoublyLinkedList {
+  // append()
+}
